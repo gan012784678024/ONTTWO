c06b5a7ef8f55cb399b8abd1099a88bdf2f3ada9
干凡 <qingliandaofan@163.com>
2019-04-26 18:03:24 +0800

    ff

---
+++ ONTF/ONTTWO.framework/Headers/ONTTWO.h
@@ -0,0 +1,45 @@
+//
+//  ONTTWO.h
+//  ONTTWO
+//
+//  Created by G 几点 on 2019/4/25.
+//  Copyright © 2019 G 几点. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ONTTWO.
+FOUNDATION_EXPORT double ONTTWOVersionNumber;
+
+//! Project version string for ONTTWO.
+FOUNDATION_EXPORT const unsigned char ONTTWOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ONTTWO/PublicHeader.h>
+
+#import <ONTTWO/ONTRpcApi.h>
+#import <ONTTWO/ONTAccount.h>
+#import <ONTTWO/ONTUtils.h>
+#import <ONTTWO/ONTBalance.h>
+#import <ONTTWO/ONTMnemonicCode.h>
+#import <ONTTWO/Oep4.h>
+#import <ONTTWO/ONT.h>
+#import <ONTTWO/NeoVM.h>
+#import <ONTTWO/ONTIdentity.h>
+
+#import <ONTTWO/ONTPublicKey.h>
+#import <ONTTWO/ONTAddress.h>
+#import <ONTTWO/ONTPrivateKey.h>
+#import <ONTTWO/MnemonicCode.h>
+#import <ONTTWO/ONTTransaction.h>
+#import <ONTTWO/ONTAttribute.h>
+#import <ONTTWO/ONTSignature.h>
+//#import <ONTTWO/SHAHash.h>
+//#import <ONTTWO/IAGAesGcm.h>
+//#import <ONTTWO/IAGCipheredData.h>
+//#import <ONTTWO/NSString+Base58.h>
+//#import <ONTTWO/NSData+Extend.h>
+//#import <ONTTWO/NSMutableData+Extend.h>
+//#import <ONTTWO/NSData+Hash.h>
+
+
+
